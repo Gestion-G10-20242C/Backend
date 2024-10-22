31e65aa62acc2d8d7aaa81c71e75c7bc91a7e66b
Aldana <aldanuchi@gmail.com>
2024-10-22 04:44:07 -0300

    feat: add Get user profile endpoint, basic implementation

---
+++ src/app/main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+
+# FastAPI app
+app = FastAPI()
+
+# Mocked temp vars
+mocked_description = "Hola! Mi nombre es Carlos Fontela, soy un desarrollador de software amante de las metodologías ágiles."
+mocked_fav_book_description = "Me gustó mucho, coincido con varios de los aspectos nombrados por el autor. Java es un lenguaje de programación de propósito general, concurrente, orientado a objetos que fue diseñado específicamente para tener tan pocas dependencias de implementación como fuera posible. UML es un lenguaje de modelado gráfico que se utiliza para especificar, visualizar, construir y documentar un sistema de software."
+
+@app.get("/users/{username}")
+def get_user_profile(username: str):
+    
+    mocked_user = {
+        'name': 'Carlos Fontela',
+        'username': 'carlosfontela',
+        'profilePicture': '[url]',
+        'description': mocked_description,
+        'favouriteBook': {
+            'title': 'Java y UML',
+            'cover': '[url]',
+            'description': mocked_fav_book_description
+        },
+        'groups': [
+            {
+                'name': 'Agile beasts',
+                'members': 14
+            },
+            {
+                'name': 'FIUBA',
+                'members': 518
+            },
+            {
+                'name': 'Borges Lovers',
+                'members': 1234
+            },
+        ]
+    }
+    
+    return mocked_user
