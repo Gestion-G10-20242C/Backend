9e5307f652aa27968e7b5ce8ebd10a854430ffb5
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2024-10-23 21:28:03 -0300

    improved doc

--- lambda_functions/cognitoSignUp/lambda_function.py
+++ lambda_functions/cognitoSignUp/lambda_function.py
@@ -5,9 +5,23 @@ from botocore.exceptions import ClientError
 
 cognito_client = boto3.client('cognito-idp')
 
-CLIENT_ID = os.environ.get('CLIENT_ID', '2i7rhsvoo43nqblk2h8ucrfa8v')
-USER_POOL_ID = os.environ.get('USER_POOL_ID', 'us-east-1_1r6FDZzId')  
+CLIENT_ID = os.environ.get('CLIENT_ID')
+USER_POOL_ID = os.environ.get('USER_POOL_ID')  
 
+"""
+    Handles user signup via AWS Cognito.
+    This function checks if the provided email already exists in the Cognito User Pool.
+    If the email exists, it returns an error response. If the email does not exist, it
+    attempts to sign up the user with the provided username, password, and email.
+    Parameters:
+    event (dict): A dictionary containing the following keys:
+        - username (str): The username for the new user.
+        - password (str): The password for the new user.
+        - email (str): The email address for the new user.
+    Returns:
+    dict: A dictionary containing the status code and body. The body is a JSON string with a message
+          and, in case of success, the userSub. In case of error, it contains an error message and error type.
+"""
 def lambda_handler(event, context):
     username = event['username']
     password = event['password']
