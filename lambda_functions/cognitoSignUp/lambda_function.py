dee65175bbde096e1f47c86c63cab70ce5927785
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2024-10-23 21:26:15 -0300

    added email already used by another user

--- lambda_functions/cognitoSignUp/lambda_function.py
+++ lambda_functions/cognitoSignUp/lambda_function.py
@@ -5,13 +5,39 @@ from botocore.exceptions import ClientError
 
 cognito_client = boto3.client('cognito-idp')
 
-CLIENT_ID = os.environ.get('CLIENT_ID')
+CLIENT_ID = os.environ.get('CLIENT_ID', '2i7rhsvoo43nqblk2h8ucrfa8v')
+USER_POOL_ID = os.environ.get('USER_POOL_ID', 'us-east-1_1r6FDZzId')  
 
 def lambda_handler(event, context):
     username = event['username']
     password = event['password']
     email = event['email']
+
+    try:
+        existing_users = cognito_client.list_users(
+            UserPoolId=USER_POOL_ID,
+            Filter=f'email="{email}"'
+        )
+        
+        if existing_users['Users']:
+            return {
+                'statusCode': 400,
+                'body': json.dumps({
+                    'message': 'Email already exists',
+                    'error': 'EmailAlreadyExists'
+                })
+            }
     
+    except ClientError as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({
+                'message': 'Error checking if email exists',
+                'error': str(e)
+            })
+        }
+    
+
     try:
         response = cognito_client.sign_up(
             ClientId=CLIENT_ID,
