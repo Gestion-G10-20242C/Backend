c9a6433ce3f9751b7c3a3053f2878288b94c1d83
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2024-11-20 14:51:22 -0300

    added handling for url encoded format (%20, eg)

--- lambda_functions/getGroupByName/lambda_function.py
+++ lambda_functions/getGroupByName/lambda_function.py
@@ -2,6 +2,8 @@ import json
 import boto3
 from boto3.dynamodb.types import TypeDeserializer
 from decimal import Decimal
+import urllib.parse
+
 
 dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
 
@@ -60,7 +62,8 @@ def get_users_by_group(group_id):
 
 def lambda_handler(event, context):
     print(event)
-    group_id = event['pathParameters']['group_id']
+    group_id = urllib.parse.unquote(event['pathParameters']['group_id'])
+
 
     try:
         # Determine if group_id is numeric
@@ -112,8 +115,5 @@ def lambda_handler(event, context):
 
 
 
-# event = {
-#     'pathParameters': {'group_id': '1731699934304'},
-#     'body': '{"description": "El mejor libro del mundo", "image_url":"https://i.pinimg.com/736x/f8/77/11/f8771136acc302740ba301d51d39cf7a.jpg", "genres": "Gabigol"}'
-# }
+# event = {'resource': '/groups/{group_id}', 'path': '/groups/Agile%20Beasts', 'httpMethod': 'GET', 'headers': {'Accept': '*/*', 'Accept-Encoding': 'gzip, deflate, br', 'Cache-Control': 'no-cache', 'Host': 'nev9ddp141.execute-api.us-east-1.amazonaws.com', 'Postman-Token': '764f01e2-028b-488e-a077-a2fc88444b7b', 'User-Agent': 'PostmanRuntime/7.42.0', 'X-Amzn-Trace-Id': 'Root=1-673e2055-713dc56059100bd32e75d4ab', 'X-Forwarded-For': '201.231.14.246', 'X-Forwarded-Port': '443', 'X-Forwarded-Proto': 'https'}, 'multiValueHeaders': {'Accept': ['*/*'], 'Accept-Encoding': ['gzip, deflate, br'], 'Cache-Control': ['no-cache'], 'Host': ['nev9ddp141.execute-api.us-east-1.amazonaws.com'], 'Postman-Token': ['764f01e2-028b-488e-a077-a2fc88444b7b'], 'User-Agent': ['PostmanRuntime/7.42.0'], 'X-Amzn-Trace-Id': ['Root=1-673e2055-713dc56059100bd32e75d4ab'], 'X-Forwarded-For': ['201.231.14.246'], 'X-Forwarded-Port': ['443'], 'X-Forwarded-Proto': ['https']}, 'queryStringParameters': None, 'multiValueQueryStringParameters': None, 'pathParameters': {'group_id': 'Agile%20Beasts'}, 'stageVariables': None, 'requestContext': {'resourceId': 'rx9rwa', 'resourcePath': '/groups/{group_id}', 'httpMethod': 'GET', 'extendedRequestId': 'Bjn9XFoOIAMEIIg=', 'requestTime': '20/Nov/2024:17:45:57 +0000', 'path': '/prod/groups/Agile%20Beasts', 'accountId': '349609960822', 'protocol': 'HTTP/1.1', 'stage': 'prod', 'domainPrefix': 'nev9ddp141', 'requestTimeEpoch': 1732124757104, 'requestId': 'fd45bbf3-1944-4c18-9e2b-18548f9ab9aa', 'identity': {'cognitoIdentityPoolId': None, 'accountId': None, 'cognitoIdentityId': None, 'caller': None, 'sourceIp': '201.231.14.246', 'principalOrgId': None, 'accessKey': None, 'cognitoAuthenticationType': None, 'cognitoAuthenticationProvider': None, 'userArn': None, 'userAgent': 'PostmanRuntime/7.42.0', 'user': None}, 'domainName': 'nev9ddp141.execute-api.us-east-1.amazonaws.com', 'deploymentId': 'bzcxkx', 'apiId': 'nev9ddp141'}, 'body': None, 'isBase64Encoded': False}
 # print(lambda_handler(event, None))
