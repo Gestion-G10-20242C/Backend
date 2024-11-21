982f64745c89af1f483c7b2f6c89c04fd3052144
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2024-11-21 17:26:23 -0300

    handling delete book given a booklist

--- lambda_functions/deleteBookInBooklist/lambda_function.py
+++ lambda_functions/deleteBookInBooklist/lambda_function.py
@@ -9,6 +9,7 @@ def clean_dynamodb_item(dynamodb_item):
     """Transform DynamoDB item format to standard JSON."""
     return {key: list(value.values())[0] for key, value in dynamodb_item.items()}
 
+
 def add_cors_headers(response):
     """Add CORS headers to response."""
     response['headers'] = {
@@ -18,44 +19,6 @@ def add_cors_headers(response):
     }
 
     return response
-
-
-def lambda_handler(event, context):
-    username = event['pathParameters']['username']
-    booklist_name = event['pathParameters']['booklist']
-    body = json.loads(event['body'])
-
-    # Obtener el item de DynamoDB
-    item = get_booklist(username, booklist_name)
-    if not item:
-        return add_cors_headers({
-            'statusCode': 404,
-            'body': json.dumps('Booklist not found')
-        })
-
-    # Validar y actualizar el libro
-    books = parse_books(item['books']['S'])
-    if body['id'] not in books:
-        return add_cors_headers({
-            'statusCode': 400,
-            'body': json.dumps('Book is not in booklist')
-        })
-
-    books.remove(body['id'])
-    updated_books = json.dumps(books)
-
-    # Actualizar el item en DynamoDB
-    if not update_books(username, booklist_name, updated_books):
-        return add_cors_headers({
-            'statusCode': 400,
-            'body': json.dumps('Error updating booklist')
-        })
-
-    item['books']['S'] = books  # Actualizar libros en el item original para el response
-    return add_cors_headers({
-        'statusCode': 200,
-        'body': json.dumps(clean_dynamodb_item(item))
-    })
 
 
 def get_booklist(username, booklist_name):
@@ -92,8 +55,47 @@ def update_books(username, booklist_name, books_json):
     return response['ResponseMetadata']['HTTPStatusCode'] == 200
 
 
-event = {
-    'pathParameters': {'username': 'gabitest', 'booklist': 'Leidos'},
-    'body': '{"id":"5e0dd8d1-4b54-4297-b6fe-488be3f481e3"}'
-}
-print(lambda_handler(event, None))
+def lambda_handler(event, context):
+    username = event['pathParameters']['username']
+    booklist_name = event['pathParameters']['booklist']
+    body = json.loads(event['body'])
+
+    # Obtener el item de DynamoDB
+    item = get_booklist(username, booklist_name)
+    if not item:
+        return add_cors_headers({
+            'statusCode': 404,
+            'body': json.dumps('Booklist not found')
+        })
+
+    # Validar y actualizar el libro
+    books = parse_books(item['books']['S'])
+    if not any(book == body['id'] for book,_,_ in books):
+        return add_cors_headers({
+            'statusCode': 400,
+            'body': json.dumps('Book is not in booklist')
+        })
+
+    books = [book for book in books if book[0] != body['id']]
+    updated_books = json.dumps(books)
+
+    # Actualizar el item en DynamoDB
+    if not update_books(username, booklist_name, updated_books):
+        return add_cors_headers({
+            'statusCode': 400,
+            'body': json.dumps('Error updating booklist')
+        })
+
+    item['books']['S'] = books  # Actualizar libros en el item original para el response
+    return add_cors_headers({
+        'statusCode': 200,
+        'body': json.dumps(clean_dynamodb_item(item))
+    })
+
+
+
+# event = {
+#     'pathParameters': {'username': 'gabitest', 'booklist': 'Leidos'},
+#     'body': '{"id":"1ae6453d-8c75-4b08-9f51-5126e88470f0"}'
+# }
+# print(lambda_handler(event, None))
