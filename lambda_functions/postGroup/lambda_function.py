3dc243ba4091bd96b0abf228f3ffc19706fc91e8
Gabriel Bedoya <gabriel.bedoya.m@gmail.com>
2024-11-26 22:53:37 -0300

    added dup filter for existing groups and cors headers

--- lambda_functions/postGroup/lambda_function.py
+++ lambda_functions/postGroup/lambda_function.py
@@ -6,6 +6,14 @@ import boto3
 MILIS = 1000
 dynamodb_client = boto3.client('dynamodb', region_name='us-east-1')
 
+def addCORSHeader(response):
+    response['headers'] = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'OPTIONS, POST'
+    }
+    return response
+
 def fetchOwnerName(username):
     response = dynamodb_client.get_item(
         TableName='UserProfiles',
@@ -26,6 +34,23 @@ def clean_dynamodb_item(dynamodb_item):
 def lambda_handler(event, context):
     username = event['pathParameters']['username']
     body = json.loads(event['body'])
+
+    existing_group_response = dynamodb_client.scan(
+        TableName='Groups',
+        FilterExpression='#group_name = :group_name',
+        ExpressionAttributeNames={
+            '#group_name': 'name'
+        },
+        ExpressionAttributeValues={
+            ':group_name': {'S': body['name']}
+        }
+    )
+
+    if existing_group_response.get('Items'):
+        return addCORSHeader({
+            'statusCode': 400,
+            'body': json.dumps('Error: Group already exists')
+        })
 
     item = {
         'id': {'N': str(int(time.time() * MILIS))},
@@ -50,23 +75,17 @@ def lambda_handler(event, context):
 
     status_code = response['ResponseMetadata']['HTTPStatusCode']
     if status_code != 200:
-       return {
-           'statusCode': 400,
-           'body': json.dumps('Error')
-       }
+        return addCORSHeader({
+            'statusCode': 400,
+            'body': json.dumps('Error')
+        })
 
 
 
-    return {
+    return addCORSHeader({
         'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Methods': 'OPTIONS, POST'
-        },
         'body': json.dumps(clean_dynamodb_item(item))
-    }
-
+    })
 # event = {
 #     'pathParameters': {'username': 'gabitest'},
 #     'body': '{"description": "El mejor grupo del mundo", "image_url":"https://i.pinimg.com/736x/f8/77/11/f8771136acc302740ba301d51d39cf7a.jpg", "genres": "Gabigol"}'
